2f161d2829933254cde67ce948c865367dcca750
Haley Acevedo <haley.acevedo2@gmail.com>
2018-10-09 11:37:50 -0500

    fixed slice/frame slider selection to maintain each others values on change; added support for other tracers

--- pet_aif_auc.py
+++ pet_aif_auc.py
@@ -14,25 +14,28 @@ def calc_auc(filename, integration_bounds):
 
 	data = [ l for l in data if float(l[0]) >= integration_bounds[0] and float(l[0]) <= integration_bounds[1] ]
 
+	if not data:
+		return np.nan
+		
 	x, y = zip(*data)
 	x = [ float(z) for z in x ]
 	y = [ float(z) for z in y ]
 	return scipy.integrate.trapz(y, x)
 
 
-def pet_aif_auc(project_dir, subject_map):
+def pet_aif_auc(project_dir, subject_map, tracer):
 	results = []
 	for subject, mapping in subject_map.items():
 		for condition, visit_id in mapping.items():
-			aiffile = '{0}/{1}/Vall/mlsiemens_Herscovitch1985_plotCaprac_fdgv{2}r1_fdgv{2}r1.csv'.format(project_dir, subject, visit_id)
+			aiffile = '{0}/{1}/Vall/mlsiemens_Herscovitch1985_plotCaprac_{2}v{3}r1_{2}v{3}r1.csv'.format(project_dir, subject, tracer, visit_id)
 			aif_auc = calc_auc(aiffile, (0,3000)) if os.path.exists(aiffile) else np.nan
 
-			petfile = '{0}/{1}/Vall/mlsiemens_Herscovitch1985_plotScannerWholebrain_fdgv{2}r1_fdgv{2}r1.csv'.format(project_dir, subject, visit_id)
+			petfile = '{0}/{1}/Vall/mlsiemens_Herscovitch1985_plotScannerWholebrain_{2}v{3}r1_{2}v{3}r1.csv'.format(project_dir, subject, tracer, visit_id)
 			pet_auc = calc_auc(petfile, (2400, 3600)) if os.path.exists(petfile) else np.nan
 
 			results.append([subject, condition, aif_auc, pet_auc])
 
-	with open('static/ppg_auc.csv', 'w', newline='') as f:
+	with open('static/{}_auc.csv'.format(tracer), 'w', newline='') as f:
 		writer = csv.writer(f)
 		writer.writerow(['subject', 'condition', 'x', 'y'])
 		writer.writerows(results)
--- pet_aif_viz.py
+++ pet_aif_viz.py
@@ -41,7 +41,6 @@ for index, row in df.iterrows():
 	if row[SUBJECT_ID] not in subject_map:
 		subject_map[row[SUBJECT_ID]] = {}
 	subject_map[row[SUBJECT_ID]][row['Condition']] = int(row[VISIT_ID])
-pet_aif_auc(app.config['PROJECT_FOLDER'], subject_map) # recalculate auc on program start (in case any underlying CSVs have changed)
 
 
 def dim_color(rgb_str):
@@ -49,13 +48,15 @@ def dim_color(rgb_str):
 
 
 # regenerate plotdata on each fig_report page load (needed to maintain "selected" subject styling)
-def get_plotdata(subject_id=None):
+def get_plotdata(tracer, subject_id=None):
+	pet_aif_auc(app.config['PROJECT_FOLDER'], subject_map, tracer) # recalculate auc on program start (in case any underlying CSVs have changed)
+
 	color_map = {
 		'red': 'rgb(255, 99, 132)',
 		'green': 'rgb(60, 179, 113)',
 		'blue': 'rgb(30, 144, 255)'
 	}
-	auc_df = pd.read_csv('static/ppg_auc.csv')
+	auc_df = pd.read_csv('static/{}_auc.csv'.format(tracer))
 	plotData = { 'datasets': [] }
 	for condition, color in [('basal', 'blue'), ('hypergly', 'red'), ('hyperins', 'green')]:
 		c_df = auc_df[auc_df.condition == condition]
@@ -74,43 +75,46 @@ def get_plotdata(subject_id=None):
 	return plotData
 
 
-def get_filename(data_type, subject, condition):
+def get_filename(data_type, subject, condition, tracer):
 	visit_id = subject_map[subject][condition]
+	tracer_match_suffix = '' if tracer =='fdg' else '?'
 
 	data_type = data_type.upper()
 	if data_type == DataType.FIG.name:
-		filename_template = '{}/Vall/fig_mlsiemens_Herscovitch1985_constructTracerState_fdgv{}r1/001.png'
+		filename_template = '{0}/Vall/fig_mlsiemens_Herscovitch1985_constructTracerState_{1}{2}v{3}r1/001.png'
 	elif data_type == DataType.TAC.name:
-		filename_template = '{0}/Vall/mlsiemens_Herscovitch1985_plotScannerWholebrain_fdgv{1}r1_fdgv{1}r1.csv'
+		filename_template = '{0}/Vall/mlsiemens_Herscovitch1985_plotScannerWholebrain_{1}{2}v{3}r1_{1}{2}v{3}r1.csv'
 	elif data_type == DataType.AIF.name:
-		filename_template = '{0}/Vall/mlsiemens_Herscovitch1985_plotCaprac_fdgv{1}r1_fdgv{1}r1.csv'
+		plot_suffix = 'Caprac' if tracer == 'fdg' else 'Aif'
+		filename_template = '{0}/Vall/mlsiemens_Herscovitch1985_plot' + plot_suffix + '_{1}{2}v{3}r1_{1}{2}v{3}r1.csv'
 	elif data_type == DataType.TAC3D.name:
-		filename_template = '{0}/Vall/fdgv{1}r1_sumt.4dfp.img'
+		filename_template = '{0}/Vall/{1}{2}v{3}r1_sumt.4dfp.img'
 	elif data_type == DataType.TAC4D.name:
-		filename_template = '{0}/Vall/fdgv{1}r1.4dfp.img'
+		filename_template = '{0}/Vall/{1}{2}v{3}r1.4dfp.img'
 
-	return filename_template.format(subject, visit_id) if filename_template else ''
+	return filename_template.format(subject, tracer, tracer_match_suffix, visit_id) if filename_template else ''
 
 
 def get_data_for_filetype(filename):
 	full_path = os.path.join(app.config['PROJECT_FOLDER'], filename)
-	if filename.endswith('.csv'):
-		data = {}
-		if os.path.exists(full_path):
-			table_html = pd.read_csv(full_path).to_html(index=False, justify='left').replace('<table border="1" class="dataframe">','<table class="table table-striped table-scrollable">')
-			data = { 'table_html': table_html, 'source': filename }
+	file_matches = glob(full_path)
+	if all(f.endswith('.csv') for f in file_matches):
+		data = []
+		for f in file_matches:
+			table_html = pd.read_csv(f).to_html(index=False, justify='left').replace('<table border="1" class="dataframe">','<table class="table table-striped table-scrollable">')
+			data.append({ 'table_html': table_html, 'source': f })
 		return data
 	else:
-		return filename if os.path.exists(full_path) else None
+		return [ os.path.relpath(f, app.config['PROJECT_FOLDER']) for f in file_matches ]
 
-def get_subject_data(data_type):
+def get_subject_data(data_type, tracer):
 	rows = []
 	for subject, condition_map in subject_map.items():
 		row = {}
 		row['subject'] = subject
 		row['data'] = {}
 		for condition in condition_map.keys():
-			filename = get_filename(data_type, subject, condition)
+			filename = get_filename(data_type, subject, condition, tracer)
 			file_data = get_data_for_filetype(filename)
 			row['data'][condition] = file_data
 		row['data'] = dict(sorted(row['data'].items(), key=lambda x: x[0])) # sort conditions alphabetically ('basal', 'hygly', 'hyins')
@@ -120,41 +124,45 @@ def get_subject_data(data_type):
 
 
 @app.route('/')
-@app.route('/<subject_id>')
-def fig_report(subject_id=None):
-	rows = get_subject_data(DataType.FIG.name)
-	return render_template('fig001.html', subject_id=subject_id, plotData=get_plotdata(subject_id), data=rows)
+@app.route('/<tracer>')
+@app.route('/<tracer>/<subject_id>')
+def fig_report(tracer='fdg', subject_id=None):
+	rows = get_subject_data(DataType.FIG.name, tracer)
+	return render_template('fig001.html', tracer=tracer, subject_id=subject_id, plotData=get_plotdata(tracer, subject_id), data=rows,
+		max_length=max([len(v) for row in rows for v in row['data'].values() ]))
 
 
-@app.route('/figdata')
-@app.route('/<subject_id>/figdata')
-def plot_report(subject_id=None):
+@app.route('/<tracer>/figdata')
+@app.route('/<tracer>/<subject_id>/figdata')
+def plot_report(tracer, subject_id=None):
 	data = {}
 	for metric in [ 'TAC', 'AIF' ]:
-		data[metric] = get_subject_data(DataType[metric].name)
-	return render_template('plot-tables.html', subject_id=subject_id, data=data)
+		data[metric] = get_subject_data(DataType[metric].name, tracer)
+	return render_template('plot-tables.html', tracer=tracer, subject_id=subject_id, data=data,
+		max_length=max([len(v) for row in data['TAC'] for v in row['data'].values() ]))
 
 
-@app.route('/figdata/tac')
-@app.route('/<subject_id>/figdata/tac')
-def tac_report(subject_id=None):
+@app.route('/<tracer>/figdata/tac')
+@app.route('/<tracer>/<subject_id>/figdata/tac')
+def tac_report(tracer, subject_id=None):
 	slice = request.args.get('slice')
 	frame = request.args.get('frame')
+
 	data = {}
 	for tab, img_type in [ ('time-integral', 'TAC3D'), ('time-series', 'TAC4D') ]:
-		data[tab] = get_subject_data(DataType[img_type].name)
+		data[tab] = get_subject_data(DataType[img_type].name, tracer)
 		if img_type == 'TAC4D':
-			sample_4dimg = os.path.join(app.config['PROJECT_FOLDER'], data[tab][0]['data']['basal'])
+			sample_4dimg = os.path.join(app.config['PROJECT_FOLDER'], data[tab][0]['data']['basal'][0])
 			max_slice, max_frame = nb.load(sample_4dimg).get_data().shape[2:]
 
-	return render_template('view-images.html', subject_id=subject_id, data=data, max_slice=max_slice, max_frame=max_frame, slice=slice,
-		frame=frame)
+	return render_template('view-images.html', tracer=tracer, subject_id=subject_id, data=data, max_slice=max_slice, max_frame=max_frame,
+		slice=slice, frame=frame, max_length=max([len(v) for row in data['time-integral'] for v in row['data'].values() ]))
 
 
-@app.route('/figdata/aif')
-@app.route('/<subject_id>/figdata/aif')
-def aif_report(subject_id=None):
-	return render_template('view-pdfs.html', subject_id=subject_id, subject_list=sorted(list(subject_map.keys())))
+@app.route('/<tracer>/figdata/aif')
+@app.route('/<tracer>/<subject_id>/figdata/aif')
+def aif_report(tracer, subject_id=None):
+	return render_template('view-pdfs.html', tracer=tracer, subject_id=subject_id, subject_list=sorted(list(subject_map.keys())))
 
 
 @app.route('/file/<path:filename>')
@@ -206,20 +214,25 @@ def gen_plot():
 	data_type = request.args.get('data_type')
 	subject = request.args.get('subject')
 	condition = request.args.get('condition')
+	tracer = request.args.get('tracer')
 
 	fig, ax = plt.subplots()
 	conditions = list(subject_map[subject].keys()) if condition == 'all' else [ condition ]
 	conditions.sort()
+	legend = []
 	for condition in conditions:
-		filename = os.path.join(app.config['PROJECT_FOLDER'], get_filename(data_type, subject, condition))
-		if not os.path.exists(filename):
-			continue
-		x, y = np.genfromtxt(filename, delimiter=',', skip_header=1, unpack=True)
-		ax.plot(x,y)
+		filenames = glob(os.path.join(app.config['PROJECT_FOLDER'], get_filename(data_type, subject, condition, tracer)))
+		for filename in filenames:
+			x, y = np.genfromtxt(filename, delimiter=',', skip_header=1, unpack=True)
+			ax.plot(x,y)
+			run = re.search('_([a-z]+(\d?))v\dr\d.csv$', filename).groups()[-1]
+			legend.append(condition + ('-' + run if run else ''))
 
+	print('l=', legend)
+	ax.set_title('{} {} for {}'.format(tracer.upper(), data_type.upper(), subject))
 	ax.set_xlabel('Time (s)')
 	ax.set_ylabel('Specific activity')
-	plt.legend(conditions)
+	plt.legend(legend)
 
 	plot = BytesIO()
 	fig.savefig(plot)
