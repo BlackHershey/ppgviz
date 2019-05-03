962fa4a592604058df73e595858faa646c6144f9
Haley Acevedo <haley.acevedo2@gmail.com>
2019-05-03 11:10:05 -0500

    only create filename lookup once (at start up)

--- pet_aif_viz.py
+++ pet_aif_viz.py
@@ -18,13 +18,56 @@ from io import BytesIO
 from matplotlib import cm
 from matplotlib import pyplot as plt
 
-def create_app(data_config_file, subject_map_file, view_config_file):
-	class DataType(Enum):
-		FIG = 1
-		TABLE = 2
-		IMG = 3
-		GRAPH = 4
+class DataType(Enum):
+	FIG = 1
+	TABLE = 2
+	IMG = 3
+	GRAPH = 4
 
+def get_filename(subject_map, data_config, view_id, tab, subject, condition, tracer):
+	visit_id = subject_map[subject][condition] if subject and condition else None
+
+	templates = next((filepath['templates'] for filepath in data_config['filepaths'] if filepath['id'].lower() == view_id.lower() \
+		and (filepath['tab'] is None or filepath['tab'].lower() == tab.lower())), None)
+	return [ template.replace('{subject}', str(subject)).replace('{session}', str(visit_id)).replace('{dataset}', str(tracer)) for template in templates ] if templates else []
+
+
+def get_data_for_filetype(project_folder, filenames, data_type):
+	file_matches = [ item for filename in filenames for item in glob(os.path.join(project_folder, filename)) ]
+	if data_type.upper() == DataType.TABLE.name:
+		data = []
+		for f in file_matches:
+			table_html = pd.read_csv(f).to_html(index=False, justify='left').replace('<table border="1" class="dataframe">','<table class="table table-striped table-scrollable">')
+			data.append({ 'table_html': table_html, 'source': f })
+		return data
+	else:
+		return [ os.path.relpath(f, project_folder) for f in file_matches ]
+
+
+def get_subject_data(subject_map, data_config, view_config, view_id, tab, tracer):
+	rows = []
+	for subject, condition_map in subject_map.items():
+		row = {}
+		row['subject'] = subject
+		row['data'] = {}
+		for condition in condition_map.keys():
+			filenames = get_filename(subject_map, data_config, view_id, tab, subject, condition, tracer)
+			file_data = get_data_for_filetype(data_config['project_folder'], filenames, view_config[view_id]['type'])
+			row['data'][condition] = file_data
+		row['data'] = dict(sorted(row['data'].items(), key=lambda x: x[0])) # sort conditions alphabetically ('basal', 'hygly', 'hyins')
+		if any(row['data'].values()):
+			rows.append(row)
+	return rows
+
+def dim_color(rgb_str):
+	return rgb_str.replace(')', ', .3)')
+
+
+def get_maxlength(data):
+	return max([len(files) for tab in data for row in data[tab] for files in row['data'].values() ], default=None)
+
+
+def create_app(data_config_file, subject_map_file, view_config_file):
 	app = Flask(__name__)
 
 	SUBJECT_ID = 'subject'
@@ -51,48 +94,16 @@ def create_app(data_config_file, subject_map_file, view_config_file):
 		subject_map[row[SUBJECT_ID]][row[CONDITION]] = int(row[VISIT_ID])
 
 
-	def dim_color(rgb_str):
-		return rgb_str.replace(')', ', .3)')
+	data_map = {}
+	for dataset in data_config['datasets']:
+		data_map[dataset] = {}
+		for id, conf in view_config.items():
+			if conf['type'] == DataType.GRAPH.name:
+				continue
 
-
-	def get_filename(view_id, tab, subject, condition, tracer):
-		visit_id = subject_map[subject][condition] if subject and condition else None
-
-		templates = next((filepath['templates'] for filepath in data_config['filepaths'] if filepath['id'].lower() == view_id.lower() \
-		 	and (filepath['tab'] is None or filepath['tab'].lower() == tab.lower())), None)
-		return [ template.replace('{subject}', str(subject)).replace('{session}', str(visit_id)).replace('{dataset}', str(tracer)) for template in templates ] if templates else []
-
-
-	def get_data_for_filetype(filenames, data_type):
-		file_matches = [ item for filename in filenames for item in glob(os.path.join(app.config['PROJECT_FOLDER'], filename)) ]
-		if data_type.upper() == DataType.TABLE.name:
-			data = []
-			for f in file_matches:
-				table_html = pd.read_csv(f).to_html(index=False, justify='left').replace('<table border="1" class="dataframe">','<table class="table table-striped table-scrollable">')
-				data.append({ 'table_html': table_html, 'source': f })
-			return data
-		else:
-			return [ os.path.relpath(f, app.config['PROJECT_FOLDER']) for f in file_matches ]
-
-
-	def get_subject_data(view_id, tab, tracer):
-		rows = []
-		for subject, condition_map in subject_map.items():
-			row = {}
-			row['subject'] = subject
-			row['data'] = {}
-			for condition in condition_map.keys():
-				filenames = get_filename(view_id, tab, subject, condition, tracer)
-				file_data = get_data_for_filetype(filenames, view_config[view_id]['type'])
-				row['data'][condition] = file_data
-			row['data'] = dict(sorted(row['data'].items(), key=lambda x: x[0])) # sort conditions alphabetically ('basal', 'hygly', 'hyins')
-			if any(row['data'].values()):
-				rows.append(row)
-		return rows
-
-
-	def get_maxlength(data, tabname):
-		return max([len(v) for row in data[tabname] for v in row['data'].values() ], default=None)
+			data_map[dataset][id] = {}
+			for tab in conf['tabs']:
+				data_map[dataset][id][tab['name']] = get_subject_data(subject_map, data_config, view_config, id, tab['name'], dataset)
 
 
 	@app.context_processor
@@ -104,7 +115,6 @@ def create_app(data_config_file, subject_map_file, view_config_file):
 	def index(tracer=default_ds, subject=None):
 		rows = None
 		for id, conf in view_config.items():
-			print(id)
 			for tab in conf['tabs']:
 				rows = get_subject_data(id, tab['name'], tracer) # find first datatype that has data (later on this will need to be view order dependent -- wont be able to rely on enum)
 				if rows:
@@ -124,7 +134,8 @@ def create_app(data_config_file, subject_map_file, view_config_file):
 		for tab in view_config[view_id]['tabs']:
 			data[tab['name']] = { 'datasets': [] }
 
-			filename = [ item for filename in get_filename(view_id, tab, None, None, tracer) for item in glob(os.path.join(app.config['PROJECT_FOLDER'], filename)) ][0]
+			filename = [ item for filename in get_filename(subject_map, data_config, view_id, tab, None, None, tracer) \
+				for item in glob(os.path.join(app.config['PROJECT_FOLDER'], filename)) ][0]
 			graph_df = pd.read_csv(filename).dropna()
 
 			for condition, color_tup in zip(labels, colors):
@@ -151,22 +162,17 @@ def create_app(data_config_file, subject_map_file, view_config_file):
 	@app.route('/<tracer>/fig/<view_id>')
 	@app.route('/<tracer>/fig/<view_id>/<subject_id>')
 	def fig_report(tracer, view_id, subject_id=None):
-		data = {}
-		for tab in view_config[view_id]['tabs']:
-			data[tab['name']] = get_subject_data(view_id, tab['name'], tracer)
-
+		data = data_map[tracer][view_id]
 		return render_template('fig_report.html', tracer=tracer, view_id=view_id, subject_id=subject_id,
-			data=data, max_length=get_maxlength(data, view_config[view_id]['tabs'][0]['name']))
+			data=data, max_length=get_maxlength(data))
 
 
 	@app.route('/<tracer>/tables/<view_id>')
 	@app.route('/<tracer>/tables/<view_id>/<subject_id>')
 	def table_report(tracer, view_id, subject_id=None):
-		data = {}
-		for tab in view_config[view_id]['tabs']:
-			data[tab['name']] = get_subject_data(view_id, tab['name'], tracer)
+		data = data_map[tracer][view_id]
 		return render_template('table_report.html', tracer=tracer, view_id=view_id, subject_id=subject_id, data=data,
-			max_length=get_maxlength(data, view_config[view_id]['tabs'][0]['name']))
+			max_length=get_maxlength(data))
 
 
 	@app.route('/<tracer>/img/<view_id>')
@@ -175,11 +181,11 @@ def create_app(data_config_file, subject_map_file, view_config_file):
 		slice = request.args.get('slice')
 		frame = request.args.get('frame')
 
-		data = {}
+		data = data_map[tracer][view_id]
+
+		# figure out shape of images being shown in each tab
 		dims = {}
 		for tab in view_config[view_id]['tabs']:
-			data[tab['name']] = get_subject_data(view_id, tab['name'], tracer)
-			# figure out shape of images being shown in tab
 			sample_img_filename = os.path.join(app.config['PROJECT_FOLDER'], list(itertools.chain.from_iterable(data[tab['name']][0]['data'].values()))[0])
 			temp = nb.load(sample_img_filename).get_data()
 			if temp.shape[-1] == 1:
@@ -190,7 +196,7 @@ def create_app(data_config_file, subject_map_file, view_config_file):
 			max_frame = temp.shape[3] if len(temp.shape) == 4 else None
 
 		return render_template('img_report.html', tracer=tracer, view_id=view_id, subject_id=subject_id, data=data, max_slice=max_slice,
-			max_frame=max_frame, slice=slice, frame=frame, dims=dims, max_length=get_maxlength(data, view_config[view_id]['tabs'][0]['name']))
+			max_frame=max_frame, slice=slice, frame=frame, dims=dims, max_length=get_maxlength(data))
 
 
 	@app.route('/file/<path:filename>')
@@ -243,7 +249,7 @@ def create_app(data_config_file, subject_map_file, view_config_file):
 		zoom_range = None
 
 		for condition in conditions:
-			filenames = [ item for filename in get_filename(view_id, data_type, subject, condition, tracer) \
+			filenames = [ item for filename in get_filename(subject_map, data_config, view_id, data_type, subject, condition, tracer) \
 				for item in glob(os.path.join(app.config['PROJECT_FOLDER'], filename)) ]
 
 			# filenames = glob(os.path.join(app.config['PROJECT_FOLDER'], ))
